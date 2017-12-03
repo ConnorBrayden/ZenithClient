d1dec768e1ebae506f67f947705d56c61b2e09dc
ConnorJang <connorjang20@gmail.com>
2017-12-02 22:23:45 -0800

    more progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { ActivityCategoryDetailComponent } from './activity-category-detail/activity-category-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventDetailComponent,
+    ActivityCategoryDetailComponent
   ],
   imports: [
     BrowserModule
