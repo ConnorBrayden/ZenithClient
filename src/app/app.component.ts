cced021ed87ee2bfc4d9226672dbc986b88a9987
ConnorJang <connorjang20@gmail.com>
2017-12-02 22:11:08 -0800

    table working with dummy data, selecting row displays data in div below table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,20 +14,26 @@ export class AppComponent {
   events = DUMMY_DATA_EVENTS;
   activityCategories = DUMMY_DATA_ACTIVITY_CATEGORIES;
 
-  event: Event = {
-    EventId: 1,
-    FromDate: new Date("2017-10-18T08:30:00"),
-    ToDate: new Date("2017-10-18T10:30:00"),
-    EnteredByUsername: "b",
-    ActivityCategoryId: 3,
-    ActivityCategory: null,
-    CreationDate: new Date("2017-09-17T10:30:00"),
-    IsActive: true,
-  };
+  selected: Event;
 
-  activityCategory: ActivityCategory = {
-    ActivityCategoryId: 1,
-    ActivityDescription: "Senior's Golf Tournament",
-    CreationDate: new Date("2017-09-17T10:30:00"),
-  }
+  onSelect(event: Event): void {
+    this.selected= event;
+}
+
+  // event: Event = {
+  //   EventId: 1,
+  //   FromDate: new Date("2017-10-18T08:30:00"),
+  //   ToDate: new Date("2017-10-18T10:30:00"),
+  //   EnteredByUsername: "b",
+  //   ActivityCategoryId: 3,
+  //   ActivityCategory: null,
+  //   CreationDate: new Date("2017-09-17T10:30:00"),
+  //   IsActive: true,
+  // };
+
+  // activityCategory: ActivityCategory = {
+  //   ActivityCategoryId: 1,
+  //   ActivityDescription: "Senior's Golf Tournament",
+  //   CreationDate: new Date("2017-09-17T10:30:00"),
+  // }
 }
