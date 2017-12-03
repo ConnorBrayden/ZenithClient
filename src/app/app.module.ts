ef812963163e224d32224df05d99978eba61309c
ConnorJang <connorjang20@gmail.com>
2017-12-02 23:13:17 -0800

    added event component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 import { ActivityCategoryDetailComponent } from './activity-category-detail/activity-category-detail.component';
+import { EventComponent } from './event/event.component';
+import { ActivityCategoryComponent } from './activity-category/activity-category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventDetailComponent,
-    ActivityCategoryDetailComponent
+    ActivityCategoryDetailComponent,
+    EventComponent,
+    ActivityCategoryComponent
   ],
   imports: [
     BrowserModule
