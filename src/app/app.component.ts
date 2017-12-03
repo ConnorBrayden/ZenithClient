26eadc1c6099efd45bc90c6c43123e9c5942c75f
ConnorJang <connorjang20@gmail.com>
2017-12-02 21:42:32 -0800

    progress with dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import {Event} from './event';
 import {ActivityCategory} from './activity-category'
+import {DUMMY_DATA_EVENTS, DUMMY_DATA_ACTIVITY_CATEGORIES} from './data/dummy-data'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  events = DUMMY_DATA_EVENTS;
+  activityCategories = DUMMY_DATA_ACTIVITY_CATEGORIES;
 
   event: Event = {
     EventId: 1,
@@ -25,5 +29,5 @@ export class AppComponent {
     ActivityCategoryId: 1,
     ActivityDescription: "Senior's Golf Tournament",
     CreationDate: new Date("2017-09-17T10:30:00"),
-}
+  }
 }
